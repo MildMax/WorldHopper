7b8c438cb7aa686f5c915b8e83a9d24ebef2c557
MildMax <44384747+MildMax@users.noreply.github.com>
2019-06-29 17:55:29 -0400

    Populated MouseScript, added MouseTriggerDetector to find when player is in vicinity. Added MouseGUI to choose between what activates mouse. NOTE -- SPIDER SPINNER SNAKE SCRIPTS ARE NOT NEW - SAME AS LAST COMMIT, I JUST MOVED THE FILES IS ALL AND GIT IS ALL LIKE 'THE FUCK, MATE'

---
+++ EnemyScripts/MouseScript.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : EnemyBase
+{
+
+    [HideInInspector]
+    public int actionIndex;
+
+    public delegate bool MoveCondition();
+    MoveCondition condition;
+
+    SpriteRenderer rend;
+    MouseTriggerDetector detector;
+
+    public float speed;
+
+    public Vector2 pos1;
+    public Vector2 pos2;
+
+    public bool isLeft = false;
+    public bool isMoving = false;
+
+    public float minWait;
+    public float maxWait;
+    float timeWait;
+    float timer = 0;
+    bool timerSet = false;
+
+    bool flipSprites = false;
+
+    private void Awake()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        detector = GetComponentInParent<MouseTriggerDetector>();
+        SetCondition();
+    }
+
+    private void Update()
+    {
+        DetectLocation();
+        FlipSprites();
+
+        if (isMoving == false)
+        {
+            isMoving = condition();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isMoving == true)
+        {
+            MoveMouse();
+            CheckArrival();
+        }
+    }
+
+    //::::::::::::::::GENERIC:::::::::::::://
+
+    private void SetCondition()
+    {
+        switch(actionIndex)
+        {
+            case 0:
+                condition = DetectContact;
+                return;
+            case 1:
+                condition = MoveTimer;
+                return;
+        }
+    }
+
+    private void MoveMouse()
+    {
+        if(isLeft == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos2, speed * Time.deltaTime);
+        }
+        else if(isLeft == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos1, speed * Time.deltaTime);
+        }
+    }
+
+    private void CheckArrival()
+    {
+        if(isLeft == true && (Vector2)transform.position == pos2)
+        {
+            isMoving = false;
+        }
+        else if(isLeft == false && (Vector2)transform.position == pos1)
+        {
+            isMoving = false;
+        }
+    }
+
+    private void DetectLocation()
+    {
+        if((Vector2)transform.position == pos1)
+        {
+            isLeft = true;
+        }
+        else if((Vector2)transform.position == pos2)
+        {
+            isLeft = false;
+        }
+    }
+
+    private void FlipSprites()
+    {
+        if (isMoving == true)
+        {
+            if (isLeft == false)
+            {
+                if (rend.flipX != false)
+                {
+                    rend.flipX = false;
+                }
+
+
+            }
+            else if (isLeft == true)
+            {
+                if (rend.flipX != true)
+                {
+                    rend.flipX = true;
+                }
+            }
+        }
+    }
+
+    //::::::::::::::::CONTACT::::::::::::://
+
+    private bool DetectContact()
+    {
+        if (detector.inVicinity == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //::::::::::::::TIMER:::::::::::::::://
+
+    private bool MoveTimer()
+    {
+        SetTimer();
+
+        if (timer >= timeWait)
+        {
+            timerSet = false;
+            timer = 0;
+            return true;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            return false;
+        }
+
+        
+    }
+
+    private void SetTimer()
+    {
+        if(timerSet == false)
+        {
+            timeWait = Random.Range(minWait, maxWait);
+            timerSet = true;
+        }
+    }
+
+}
---
+++ EnemyScripts/MouseTriggerDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTriggerDetector : MonoBehaviour
+{
+    [HideInInspector]
+    public bool inVicinity;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            inVicinity = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            inVicinity = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            inVicinity = false;
+        }
+    }
+}
