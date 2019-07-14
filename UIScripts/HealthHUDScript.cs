da653c15b1b9e79d4aa76b528a392163d0f40917
MildMax <44384747+MildMax@users.noreply.github.com>
2019-07-14 15:15:49 -0400

    Created UIScripts folder. Created HeartHUDScript to handle displaying hearts on screen. Created HeartBeatScript to manually animate hearts using sine wave.

---
+++ UIScripts/HealthHUDScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHUDScript : MonoBehaviour
+{
+    //set health to pull from player(0 - 6)
+    public int health;
+
+    public Sprite heartEmpty;
+    public Sprite heartHalf;
+    public Sprite heartFull;
+
+    Image[] hearts;
+    Vector2 startPos;
+
+    int oldHealth = 100;
+
+    private void Awake()
+    {
+        hearts = GetComponentsInChildren<Image>();
+        //Debug.Log(hearts.Length);
+        SetHearts();
+    }
+
+    private void Update()
+    {
+        SetHearts();
+    }
+
+    private void SetHearts()
+    {
+        if(oldHealth != health)
+        {
+            SetEmptyHearts(health);
+            SetFullHearts(health);
+
+            switch(health)
+            {
+                case 1:
+                    hearts[0].sprite = heartHalf;
+                    break;
+                case 3:
+                    hearts[1].sprite = heartHalf;
+                    break;
+                case 5:
+                    hearts[2].sprite = heartHalf;
+                    break;
+
+            }
+
+            oldHealth = health;
+        }
+    }
+
+    private void SetEmptyHearts(int h)
+    {
+        if (h < 0) h = 0;
+
+        if (h % 2 != 0 || h == 1) --h;
+
+        h /= 2;
+
+        for(int i = h; i < hearts.Length; ++i)
+        {
+            hearts[i].sprite = heartEmpty;
+        }
+    }
+
+    private void SetFullHearts(int h)
+    {
+        if (h > 6) h = 6;
+
+        if (h % 2 != 0 || h == 1) ++h;
+
+        h /= 2;
+
+        for(int i = 0; i < h; ++i)
+        {
+            hearts[i].sprite = heartFull;
+        }
+    }
+}
---
+++ UIScripts/HeartBeatScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartBeatScript : MonoBehaviour
+{
+    public Sprite heartEmpty;
+    public Sprite heartHalf;
+    public Sprite heartFull;
+
+    public float beatModifier;
+    public float frequencyModifier;
+
+    Image heart;
+    RectTransform rect;
+    public Vector2 heartSize;
+    public Vector2 startPosition;
+
+    private void Awake()
+    {
+        heart = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+        heartSize = heart.rectTransform.sizeDelta;
+        startPosition = rect.localPosition;
+    }
+
+    private void Update()
+    {
+        HeartBeat();
+    }
+
+    private void LateUpdate()
+    {
+        SetHeartPosition();
+    }
+
+    private void HeartBeat()
+    {
+        if(heart.sprite == heartEmpty)
+        {
+            heart.rectTransform.sizeDelta = heartSize;
+        }
+        else if(heart.sprite == heartFull || heart.sprite == heartHalf)
+        {
+            float beat = Mathf.Sin(Time.time * frequencyModifier);
+            heart.rectTransform.sizeDelta = new Vector2(heartSize.x + beatModifier * beat, heartSize.y + beatModifier * beat);
+        }
+    }
+
+    private void SetHeartPosition()
+    {
+        float x = startPosition.x - ((heart.rectTransform.sizeDelta.x - heartSize.x) / 2);
+        float y = startPosition.y - ((heart.rectTransform.sizeDelta.x - heartSize.y) / 2);
+
+        Debug.Log("X: " + (heart.rectTransform.sizeDelta.x - heartSize.x));
+        Debug.Log("Y: " + (heart.rectTransform.sizeDelta.x - heartSize.y));
+
+        rect.localPosition = new Vector2(x, y);
+    }
+
+
+}
