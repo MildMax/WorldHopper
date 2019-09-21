4e6a76d68dfc5278e7e6e47e7eaab5c0dbf0ec88
MildMax <44384747+MildMax@users.noreply.github.com>
2019-09-21 18:26:48 -0400

    Added LoadScript to handle loading scenes. make all scene loading functions in this script.

---
+++ UIScripts/LoadScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScript : MonoBehaviour
+{
+    public void LoadCurrentScene()
+    {
+        Debug.Log("Loading current scene!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMainScreen()
+    {
+        //once the main screen has a scene, load to that
+    }
+}
