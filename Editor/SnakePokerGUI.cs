97a2ab0545b80567a703e81e21aa222e0218853d
MildMax <44384747+MildMax@users.noreply.github.com>
2019-10-24 16:38:17 -0400

    All the old stuff now listed under Editor.

---
+++ Editor/SnakePokerGUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SnakePokerScript))]
+public class SnakePokerGUI : Editor
+{
+    string[] options = { "Continuous", "Timed", "Proximity", "Up" };
+    SerializedProperty actionIndex;
+
+    private void OnEnable()
+    {
+        actionIndex = serializedObject.FindProperty("actionIndex");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Action:", GUILayout.Width(45));
+        actionIndex.intValue = EditorGUILayout.Popup(actionIndex.intValue, options, GUILayout.Width(75));
+
+        GUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Editor/TriggerEventListenerGUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TriggerEventListener))]
+public class TriggerEventListenerGUI : Editor
+{
+    string[] worlds = { "1", "2", "3", "4" };
+    SerializedProperty worldNum;
+
+    private void OnEnable()
+    {
+        worldNum = serializedObject.FindProperty("worldNum");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("World Num:", GUILayout.Width(70));
+        worldNum.intValue = EditorGUILayout.Popup(worldNum.intValue, worlds, GUILayout.Width(100));
+
+        GUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
